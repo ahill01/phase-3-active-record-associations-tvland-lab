e807b21d9dca690371bda1d52876e0acf82e530e
Allison <amh03160@gmail.com>
2022-04-11 09:30:11 -0500

    commit tree bb2f05188f638ecff92c4b602ab7c7b75eb4dbac

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,9 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+      create_table :actors do |t| # we get a block variable here for the table
+        t.string :first_name
+        t.string :last_name
+    end
   end
 end
